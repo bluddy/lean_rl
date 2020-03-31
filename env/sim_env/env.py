a0bd7dab8d172f866cfdb7a006a2a83db2de525a
Yotam Barnoy <yotambarnoy@gmail.com>
2020-03-31 17:39:12 -0400

    sim_env: fix

--- env/sim_env/env.py
+++ env/sim_env/env.py
@@ -142,8 +142,8 @@ class State(object):
         # Handle older data
         if len(data) > index:
             self.tissue_corners = []
-            self.tissue_corners.append(np.array(data[index:index+3]); index += 3
-            self.tissue_corners.append(np.array(data[index:index+3]); index += 3
+            self.tissue_corners.append(np.array(data[index:index+3])); index += 3
+            self.tissue_corners.append(np.array(data[index:index+3])); index += 3
 
 
 class Environment(common_env.CommonEnv):
