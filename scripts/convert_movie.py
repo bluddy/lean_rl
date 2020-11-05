aa2e6ba9e52cdeae6be9f1ffb4581cbd9e1b0795
Yotam Barnoy <yotambarnoy@gmail.com>
2020-11-05 16:34:16 -0500

    handle resizing for stereo

--- scripts/convert_movie.py
+++ scripts/convert_movie.py
@@ -32,25 +32,50 @@ def run(args):
         images.append(image)
     pipe.stdout.close()
 
-    if args.out == 'mono':
-        tgt_width = height
+    if args.mode == 'mono':
+        tgt_width_end = height
+        tgt_start = 0
+        suffix = 'm'
+    elif args.mode == 'stereo':
+        tgt_width_end = height * 2
+        tgt_start = 0
+        suffix = 'st'
+    elif args.mode == 'depth':
+        tgt_width_end = height * 2 * 3
+        tgt_start = height 
+        suffix = 'd'
 
-    # Create black image
-    x_off = (args.out_width - tgt_width) / 2
-    y_off = (args.out_height - height) / 2
+    tgt_width = tgt_width_end - tgt_start
+
+    # Check if resize
+    if tgt_width > args.out_width:
+        resize = True
+        ratio = float(args.out_width) / tgt_width
+        resize_height = int(args.out_height * ratio)
+        x_off = 0
+        y_off = (args.out_height - resize_height) / 2
+    else:
+        # Create black image
+        x_off = (args.out_width - tgt_width) / 2
+        y_off = (args.out_height - height) / 2
+
     bg = np.zeros((args.out_height, args.out_width, 3), dtype=np.uint8)
 
     # Reshape and save images
-    if args.out == 'mono':
-        for i, image in enumerate(images):
-            img = bg.copy()
-            img[y_off:-y_off, x_off:-x_off, :] = image[:,0:tgt_width,:]
-            #img = image[:,0:tgt_width,:]
+    for i, image in enumerate(images):
+        bg2 = bg.copy()
+        if resize:
+            img = image[:,tgt_start:tgt_width_end,:]
             img = Image.fromarray(img)
-            img.save('_img{:05d}.png'.format(i))
+            img = img.resize((args.out_width, resize_height))
+            img = np.asarray(img)
+            bg2[y_off:-y_off, :, :] = img
+        else:
+            bg2[y_off:-y_off, x_off:-x_off, :] = image[:,tgt_start:tgt_width_end,:]
+        img = Image.fromarray(bg2)
+        img.save('_img{:05d}.png'.format(i))
 
-    out_file = os.path.splitext(args.file)[0] + '_out.mp4'
-    out_file2 = os.path.splitext(args.file)[0] + '_out2.mp4'
+    out_file = os.path.splitext(args.file)[0] + '_' + suffix + '.mp4'
     if os.path.isfile(out_file):
         os.remove(out_file)
 
@@ -59,7 +84,6 @@ def run(args):
     if len(filelist) > 0:
         cmd = 'cat {} | ffmpeg -f image2pipe -r 30 -vcodec png -i - -f lavfi -i anullsrc -vcodec h264 -preset ultrafast -pix_fmt yuv420p -g 1 -crf 1 -shortest {}'.format(pattern, out_file)
         os.system(cmd)
-        #cmd = 'ffmpeg -i {} -f lavfi -i anullsrc -c:v copy -c:a aac -shortest {}'.format(out_file2, out_file)
         for f in filelist:
             os.remove(f)
 
@@ -67,7 +91,7 @@ def run(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('file', help='movie file to load')
-    parser.add_argument('--out', help='mono|stereo|depth', default='mono')
+    parser.add_argument('--mode', help='mono|stereo|depth', default='mono')
     parser.add_argument('--size', default=448, type=int)
     parser.add_argument('--width-mult', type=int, default=3)
     parser.add_argument('--out-width', type=int, default=720)
