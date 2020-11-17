c17eef97445b076aaa7173307b89bf851c7b6f8f
Yotam Barnoy <yotambarnoy@gmail.com>
2020-11-17 15:49:28 -0500

    lighting shader work

--- env/needle3d/graphics.py
+++ env/needle3d/graphics.py
@@ -91,34 +91,60 @@ class LightingShader(object):
     uniform mat4 view;
     uniform mat4 projection;
 
+    out vec3 Normal;
+    out vec3 FragPos;
+
     void main()
     {
-    gl_Position = projection * view * model * vec4(aPos, 1.0);
+        gl_Position = projection * view * model * vec4(aPos, 1.0);
+
+        // Calculate world pos of vertex (to get interpolated fragment pos for triangle)
+        FragPos = vec3(model * vec4(aPos, 1.0));
+
+        Normal = aNormal; // Passthrough
     }
     """
 
     fragment_shader = """
+
     #version 330
+
     out vec4 FragColor;
+
+    in vec3 Normal;
+    in vec3 FragPos;
 
     uniform float ambientStrength;
     uniform vec4 objectColor;
-    uniform vec3 ambientLightColor;
+    uniform vec3 lightColor;
+    uniform vec3 lightPos;
 
     void main()
     {
-        vec3 ambient = ambientStrength * ambientLightColor;
-        FragColor = vec4(ambient, 1.0) * objectColor;
+        // Ambient light
+        vec3 ambient = ambientStrength * lightColor;
+
+        // Calculate diffuse light
+        vec3 norm = normalize(Normal);
+        vec3 lightDir = normalize(lightPos - FragPos);
+        float diff = max(dot(norm, lightDir), 0.0);
+        //float diff = max(dot(norm, lightDir), 0.0);
+        vec3 diffuse = diff * lightColor;
+
+        FragColor = vec4(ambient + diffuse, 1.0) * objectColor;
+        //FragColor = vec4(ambient, 1.0) * objectColor;
     }
     """
+
     def __init__(self):
         self.shader = gl_shaders.compileProgram(
             gl_shaders.compileShader(self.vertex_shader, gl.GL_VERTEX_SHADER),
             gl_shaders.compileShader(self.fragment_shader, gl.GL_FRAGMENT_SHADER)
         )
         self.ambient_str_loc = gl.glGetUniformLocation(self.shader, 'ambientStrength')
-        self.ambient_color_loc = gl.glGetUniformLocation(self.shader, 'ambientLightColor')
+        self.light_color_loc = gl.glGetUniformLocation(self.shader, 'lightColor')
         self.obj_color_loc = gl.glGetUniformLocation(self.shader, 'objectColor')
+        self.light_pos_loc = gl.glGetUniformLocation(self.shader, 'lightPos')
 
         self.model_loc = gl.glGetUniformLocation(self.shader, 'model')
         self.view_loc = gl.glGetUniformLocation(self.shader, 'view')
@@ -126,7 +152,8 @@ class LightingShader(object):
 
     def set_default_values(self):
         self.set_ambient_strength(0.2)
-        self.set_ambient_color((1.0, 1.0, 1.0))
+        self.set_light_color((1.0, 1.0, 1.0))
+        self.set_light_pos((0,0,20))
 
     def use(self, on:bool):
         if on:
@@ -138,11 +165,14 @@ class LightingShader(object):
     def set_object_color(self, color):
         gl.glUniform4f(self.obj_color_loc, *color)
 
-    def set_ambient_color(self, color):
-        gl.glUniform3f(self.ambient_color_loc, *color)
+    def set_light_color(self, color):
+        gl.glUniform3f(self.light_color_loc, *color)
 
     def set_ambient_strength(self, x):
         gl.glUniform1f(self.ambient_str_loc, x)
+
+    def set_light_pos(self, vec):
+        gl.glUniform3f(self.light_pos_loc, *vec)
 
     def set_model(self, model_mat):
         gl.glUniformMatrix4fv(self.model_loc, 1, gl.GL_FALSE, glm.value_ptr(model_mat))
@@ -152,6 +182,8 @@ class LightingShader(object):
 
     def set_proj(self, model_mat):
         gl.glUniformMatrix4fv(self.proj_loc, 1, gl.GL_FALSE, glm.value_ptr(model_mat))
+
+
 
 LINES=0
 TRIANGLES=1
@@ -181,7 +213,11 @@ class OpenGLObject(object):
 
         gl.glBindVertexArray(self.vao)
         if self.primitive == TRIANGLES:
-            gl.glDrawElements(gl.GL_TRIANGLES, len(self.indices), gl.GL_UNSIGNED_INT, ctypes.c_void_p(0))
+            if self.indices is not None:
+                gl.glDrawElements(gl.GL_TRIANGLES, len(self.indices), gl.GL_UNSIGNED_INT, ctypes.c_void_p(0))
+            else:
+                gl.glDrawArrays(gl.GL_TRIANGLES, 0, len(self.vertices))
+
         elif self.primitive == LINES:
             gl.glDrawArrays(gl.GL_LINES, 0, len(self.vertices))
         else:
@@ -269,7 +305,10 @@ class OpenGLRenderer(object):
         img = np.frombuffer(img_buf, np.uint8).reshape(self.res[1], self.res[0], 3)[::-1]
         return img
 
-    def create_object(self, vertices, indices=None, shader=DEFAULT, primitive=TRIANGLES):
+    def create_object(self, vertices, indices=None, shader=DEFAULT, primitive=TRIANGLES, stride=0):
+        '''
+        stride: how far to move between vertices
+        '''
         # Create a new VAO (Vertex Array Object) and bind it
         vao = gl.glGenVertexArrays(1)
         gl.glBindVertexArray(vao)
@@ -280,7 +319,7 @@ class OpenGLRenderer(object):
         gl.glBufferData(gl.GL_ARRAY_BUFFER, vertices.nbytes, vertices, gl.GL_STATIC_DRAW)
 
         # Describe the position data layout in the buffer
-        gl.glVertexAttribPointer(0, 3, gl.GL_FLOAT, False, 0, ctypes.c_void_p(0))
+        gl.glVertexAttribPointer(0, 3, gl.GL_FLOAT, False, stride, ctypes.c_void_p(0))
         gl.glEnableVertexAttribArray(0)
 
         buffers=[vbo, vao]
@@ -307,68 +346,77 @@ class OpenGLRenderer(object):
 
     def create_rectangle(self, shader=DEFAULT):
         vertices = np.array(
-                [ 0.5, 0.5, 0.,
-                  -0.5, 0.5, 0.,
-                  0.5, -0.5, 0.,
-                  -0.5, -0.5, 0.,
+                [ 0.5, 0.5, 0., 0., 0., 1.,
+                  -0.5, 0.5, 0., 0., 0., 1.,
+                  0.5, -0.5, 0., 0., 0., 1.,
+                  -0.5, -0.5, 0., 0., 0., 1.,
                 ], dtype=np.float32)
         # CCW
         indices = np.array(
                 [0, 1, 2,
                  2, 1, 3,
                  ], dtype=np.uint32)
-        return self.create_object(vertices, indices, shader=shader)
+        return self.create_object(vertices, indices, shader=shader, stride=6 * 4)
 
     def create_cube(self, shader=DEFAULT):
+        # Into the screen is neg
         #1 -----0
-        #|\     |\
+        #|\   F |\
         #| \    | \
-        #|  5---+--4
-        #|  |   |  |
-        #3--+---2  |
-        # \ |    \ |
+        #|  5D--+--4
+        #|A |   | C|
+        #3--+--B2  |
+        # \ | E  \ |
         #  \|     \|
         #   7------6
         p = 0.5
         n = -0.5
+        v0 = [p, p, n]
+        v1 = [n, p, n]
+        v2 = [p, n, n]
+        v3 = [n, n, n]
+        v4 = [p, p, p]
+        v5 = [n, p, p]
+        v6 = [p, n, p]
+        v7 = [n, n, p]
+
+        nA = [-1, 0, 0]
+        nB = [0, 0, 1]
+        nC = [1, 0, 0]
+        nD = [0, 0, -1]
+        nE = [0, -1, 0]
+        nF = [0, 1, 0]
+
         vertices = [
-                p, p, n, # 0
-                n, p, n, # 1
-                p, n, n, # 2
-                n, n, n, # 3
-                p, p, p, # 4
-                n, p, p, # 5
-                p, n, p, # 6
-                n, n, p, # 7
-                ]
+                *v0, *nF, *v2, *nF, *v3, *nF,
+                *v3, *nF, *v1, *nF, *v0, *nF,
+                *v1, *nA, *v3, *nA, *v7, *nA,
+                *v7, *nA, *v5, *nA, *v1, *nA,
+                *v5, *nB, *v7, *nB, *v6, *nB,
+                *v6, *nB, *v4, *nB, *v5, *nB,
+                *v4, *nC, *v6, *nC, *v2, *nC,
+                *v2, *nC, *v0, *nC, *v4, *nC,
+                *v0, *nD, *v2, *nD, *v3, *nD,
+                *v3, *nD, *v1, *nD, *v0, *nD,
+                *v2, *nE, *v6, *nE, *v7, *nE,
+                *v7, *nE, *v3, *nE, *v2, *nE,
+                *v0, *nF, *v1, *nF, *v5, *nF,
+                *v5, *nF, *v4, *nF, *v0, *nF,
+        ]
         vertices = np.array(vertices, dtype=np.float32)
-        # CCW
-        indices = np.array(
-                [0, 4, 6, # right side
-                 6, 2, 0,
-                 5, 1, 3, # left side
-                 3, 7, 5,
-                 1, 0, 2, # back side
-                 2, 3, 1,
-                 4, 5, 7, # front
-                 7, 6, 4,
-                 0, 1, 5, # top
-                 5, 4, 0,
-                 3, 2, 6, # bottom
-                 6, 7, 3
-                 ], dtype=np.uint32)
-        return self.create_object(vertices, indices, shader=shader)
+        return self.create_object(vertices, shader=shader, stride=6 * 4) # 6 floats
+    #TODO: format with normals
 
     def create_triangle(self, shader=DEFAULT):
         vertices = np.array(
-                [ 0.5,  0.5, 0.,
-                  -0.5,  0.5, 0.,
-                  0., -0.5, 0.,
+                [ 0.5,  0.5, 0., 0., 0., 1.,
+                  -0.5,  0.5, 0., 0., 0., 1.,
+                  0., -0.5, 0., 0., 0., 1.,
                 ], dtype=np.float32)
         indices = np.array(
                 [0, 1, 2],
                 dtype=np.uint32)
-        return self.create_object(vertices, indices, shader=shader)
+        return self.create_object(vertices, indices, shader=shader, stride=6*4)
 
     def create_wireframe_rec(self, shader=DEFAULT):
         vertices = np.array(
